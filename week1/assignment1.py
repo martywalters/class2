6e8792656d9a0301e7479b9fff6ff0e83c0a0afc
marty <martywalters@yahoo.com>
2024-02-23 10:13:28 -0800

    changes

--- week1/assignment1.py
+++ week1/assignment1.py
@@ -4,59 +4,64 @@ Created on Thu Feb 22 16:37:46 2024
 
 @author: walters
 """
+#Part 1.
 import datetime
-
-def get_user_input():
-    name = input("What is your name? ")
-    age = int(input("What is your age? "))
-    current_year = datetime.datetime.now().year
-    years_to_100 = 100 - age
-    year_of_100 = current_year + years_to_100
-    print(f"Hello, {name}! You will turn 100 years old in the year {year_of_100}.")
-
-def common_elements_set(a, b):
-    # Convert lists to sets to find common elements
+def user_input_name_age():
+    name = input("name: ")
+    age = int(input("Age: "))
+    curr_year = datetime.datetime.now().year
+    to_100 = 100 - age
+    year_100 = curr_year + to_100
+    print(name + " you will turn 100 in the year "+ str(year_100))
+ 
+#Part 2.
+def common_elements(a, b):
     set_a = set(a)
     set_b = set(b)
-    common_set = set_a & set_b  # Intersection of sets
+   #zcommon_set = set_a & set_b 
     z = set_b.intersection(set_a)
-    print("common elements",list(z))
-    return list(common_set)  # Convert back to a list
+    return list(z)  # Convert back to a list
 
-def reverse_words_list_comprehension(input_string):
+#Part 3.
+def backwards_order(input_string):
     words = input_string.split()
-    reversed_string = " ".join(words[::-1])
-    return reversed_string
+    reversed = " ".join(words[::-1])
+    return reversed
 
-def extract_first_and_last_elements(input_list):
-    # Get the first and last elements using list slicing
-    result = input_list[::len(input_list) - 1]
-    return result
-
-def create_new_string(s1, s2):
-    # Get the first, middle, and last characters from each input string
-    new_string = s1[0]+ s2[0] + s1[len(s1) // 2] + s2[len(s2) // 2]  + s1[-1] + s2[-1]
-    return new_string
-
+#Part 4
+def extract_first_last(in_list):
+    short_list = in_list[::len(in_list) - 1]
+    return short_list
+#Part 5
+def create_string(s1, s2):
+    new_str = s1[0]+ s2[0] + s1[len(s1) // 2] + s2[len(s2) // 2]  + s1[-1] + s2[-1]
+    return new_str
 
 
+
+
+# Part 1.   Get name and age display message of name 
+# and age they will turn 100
+user_input_name_age()
+
+# Part 2. 
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+common_elements = common_elements(a, b)
+print("Common between the list  (without duplicates):", common_elements)
 
-get_user_input()
+# Part 3.
+instring = input("Enter long string containing multiple words: ")
+reversed_str = backwards_order(instring)
+print("String reversed:", reversed_str)
 
-common_elements = common_elements_set(a, b)
-print("Common elements (without duplicates):", common_elements)
-
-user_input = input("Enter a long string with multiple words: ")
-reversed_result = reverse_words_list_comprehension(user_input)
-print("Reversed string:", reversed_result)
-
+#Part 4
 a = [5, 10, 15, 20, 25]
-new_list = extract_first_and_last_elements(a)
-print("New list with first and last elements:", new_list)
+short_list = extract_first_last(a)
+print("First Last :", short_list)
 
+#Part 5
 s1 = "America"
 s2 = "Japan"
-result = create_new_string(s1, s2)
-print("New string:", result)
+result = create_string(s1, s2)
+print("Modified String:", result)
