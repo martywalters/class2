a16bc1b052874e7ab2925a8fcff663cce5c0141d
marty <martywalters@yahoo.com>
2024-02-24 19:51:53 -0800

    finished

--- week1/assignment1.py
+++ week1/assignment1.py
@@ -7,12 +7,12 @@ Created on Thu Feb 22 16:37:46 2024
 #Part 1.
 import datetime
 def user_input_name_age():
-    name = input("name: ")
-    age = int(input("Age: "))
+    name = input("Enter name: ")
+    age = int(input("Enter age: "))
     curr_year = datetime.datetime.now().year
     to_100 = 100 - age
     year_100 = curr_year + to_100
-    print(name + " you will turn 100 in the year "+ str(year_100))
+    print(name + " you will turn 100 in the year "+ str(year_100) + " (if your birthday is passed)")
  
 #Part 2.
 def common_elements(a, b):
@@ -42,25 +42,30 @@ def create_string(s1, s2):
 
 # Part 1.   Get name and age display message of name 
 # and age they will turn 100
+print("Part 1")
 user_input_name_age()
 
 # Part 2. 
+print("Part 2")
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 common_elements = common_elements(a, b)
 print("Common between the list  (without duplicates):", common_elements)
 
 # Part 3.
+print("Part 3")
 instring = input("Enter long string containing multiple words: ")
 reversed_str = backwards_order(instring)
 print("String reversed:", reversed_str)
 
 #Part 4
+print("Part 4")
 a = [5, 10, 15, 20, 25]
 short_list = extract_first_last(a)
 print("First Last :", short_list)
 
 #Part 5
+print("Part 5")
 s1 = "America"
 s2 = "Japan"
 result = create_string(s1, s2)
