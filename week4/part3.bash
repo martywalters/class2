adb306d23192500444d0793dc8779889e79324bd
walters <walters@workbook.esri.com>
2024-03-16 09:58:08 -0700

    final 4

--- week4/part3.bash
+++ week4/part3.bash
@@ -1,17 +1,26 @@
-echo '*** Start ServerSocket in seperate command window'
+echo '*** Start ServerSocket in seperate command window use:'
+echo 'python3 ServerSocketFileXfer.py '
 #open seperate command window and run ServerSocktFileXfer.py 
+read -p "Press enter to continue"
 echo '*** Remove old team_data.pkl file'
 rm team_data.pkl
 echo '*** run python code to Pickle It'
+
 python3 -c 'from part3 import pickle_it;pickle_it()'
+
 echo '*** run python code to move Pickle file to Client Folder'
+
 python3 -c 'from part3 import move_pickle;move_pickle()'
+
 echo '*** change to client directory'
 cd client 
 echo '*** run client socket to transfer file back to server '
+
 python3 ClientSocketFileXfer.py 
+
 echo '*** change back to main folder File transfer is finished'
 cd ..
 echo '*** unpickle the transfer file'
+
 python3 -c 'from part3 import unpickle_it;unpickle_it()'
 
